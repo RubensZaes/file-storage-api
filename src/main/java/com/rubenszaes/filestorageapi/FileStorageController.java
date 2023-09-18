cbf569c3b364c92849aa63c0c906416311ba6301
RubensZaes <rubenszaes@gmail.com>
2023-09-18 11:18:21 -0300

    :sparkles: feat Endpoint de Upload, Download e Lista de arquivos

--- src/main/java/com/rubenszaes/filestorageapi/FileStorageController.java
+++ src/main/java/com/rubenszaes/filestorageapi/FileStorageController.java
@@ -1,2 +1,81 @@
-package com.rubenszaes.filestorageapi;public class FileStorageController {
+package com.rubenszaes.filestorageapi;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/api/files")
+public class FileStorageController {
+    private final Path fileStorgeLocation;
+
+    public FileStorageController(FileStorageProperties fileStorageProperties) {
+        this.fileStorgeLocation = Paths.get(fileStorageProperties.getUploadDir())
+                                    .toAbsolutePath()
+                                    .normalize();
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            Path targetLocation = fileStorgeLocation.resolve(fileName);
+            file.transferTo(targetLocation);
+            String fileDownloadUri = ServletUriComponentsBuilder
+                                        .fromCurrentContextPath()
+                                        .path("/api/files/download/")
+                                        .path(fileName)
+                                        .toUriString();
+            return ResponseEntity.ok("Upload completed! Download link: " + fileDownloadUri);
+        } catch (IOException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
+        Path filePath = fileStorgeLocation.resolve(fileName).normalize();
+
+        try {
+            Resource resource = new UrlResource(filePath.toUri());
+            String contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+
+            if (contentType == null) contentType = "application/octet-stream";
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<String>> listFiles() throws IOException {
+        List<String> fileNames = Files.list(fileStorgeLocation)
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(fileNames);
+    }
 }
--- src/main/java/com/rubenszaes/filestorageapi/FileStorageProperties.java
+++ src/main/java/com/rubenszaes/filestorageapi/FileStorageProperties.java
@@ -1,2 +1,18 @@
-package com.rubenszaes.filestorageapi;public class FileStorageProperties {
+package com.rubenszaes.filestorageapi;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+public class FileStorageProperties {
+    private String uploadDir;
+
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
 }
